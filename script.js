e30a2a44a088be2ad285b2428d07350d5f7543f4
levilu800b <levilu800b@gmail.com>
2022-11-25 15:41:18 +0000

    js start

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-window.addEventListener('load', function() {
-    let long;
-    let lat;
+window.addEventListener('load', function () {
+	let long;
+	let lat;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-        })
-    }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+
+            // // API call from OpenWeatherMap to get weather data
+            // const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1a6ef8297115d4743cce36a3b48dbc3b`;
+
+            // fetch(api)
+            //     .then((response) => {
+            //         return response.json();
+            //     })
+            //     .then((data) => {
+            //         console.log(data);
+            //     });
+		});
+	}
 });
