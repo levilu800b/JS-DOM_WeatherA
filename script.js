0d47fac1725c87fea99f7ce8283bd7b9344cb60a
levilu800b <levilu800b@gmail.com>
2022-11-26 15:34:36 +0000

    temp changes

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener('load', function () {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition((position) => {
@@ -26,8 +28,22 @@ window.addEventListener('load', function () {
                     temperatureDegree.textContent = temp;
                     temperatureDescription.textContent = description;
                     locationTimezone.textContent = name;
+                    // Formula for Celsius
+                    let celsius = (temp - 32) * (5 / 9);
+
                     // Set Icon
                     setIcons(icon, document.querySelector('.icon'));
+
+                    // Change temperature to Celsius/Farenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temp;
+                        }
+                    });
                 });
 		});
 	}
