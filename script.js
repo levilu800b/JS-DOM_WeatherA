4472a48dd9ca36cab5f789f3d95f3df74c21c67e
levilu800b <levilu800b@gmail.com>
2022-11-30 20:57:40 +0000

    html

--- script.js
+++ script.js
@@ -1,58 +1,54 @@
-window.addEventListener('load', function () {
-	let long;
-	let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('.temperature');
-    const temperatureSpan = document.querySelector('.temperature span');
+// let weather = {
+// 	apiKey: 'API KEY GOES HERE',
+// 	fetchWeather: function (city) {
+// 		fetch(
+// 			'https://api.openweathermap.org/data/2.5/weather?q=' +
+// 				city +
+// 				'&units=metric&appid=' +
+// 				this.apiKey,
+// 		)
+// 			.then((response) => {
+// 				if (!response.ok) {
+// 					alert('No weather found.');
+// 					throw new Error('No weather found.');
+// 				}
+// 				return response.json();
+// 			})
+// 			.then((data) => this.displayWeather(data));
+// 	},
+// 	displayWeather: function (data) {
+// 		const { name } = data;
+// 		const { icon, description } = data.weather[0];
+// 		const { temp, humidity } = data.main;
+// 		const { speed } = data.wind;
+// 		document.querySelector('.city').innerText = 'Weather in ' + name;
+// 		document.querySelector('.icon').src =
+// 			'https://openweathermap.org/img/wn/' + icon + '.png';
+// 		document.querySelector('.description').innerText = description;
+// 		document.querySelector('.temp').innerText = temp + '°C';
+// 		document.querySelector('.humidity').innerText =
+// 			'Humidity: ' + humidity + '%';
+// 		document.querySelector('.wind').innerText =
+// 			'Wind speed: ' + speed + ' km/h';
+// 		document.querySelector('.weather').classList.remove('loading');
+// 		document.body.style.backgroundImage =
+// 			"url('https://source.unsplash.com/1600x900/?" + name + "')";
+// 	},
+// 	search: function () {
+// 		this.fetchWeather(document.querySelector('.search-bar').value);
+// 	},
+// };
 
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition((position) => {
-			long = position.coords.longitude;
-			lat = position.coords.latitude;
+// document.querySelector('.search button').addEventListener('click', function () {
+// 	weather.search();
+// });
 
-            // API call from OpenWeatherMap to get weather data
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1a6ef8297115d4743cce36a3b48dbc3b`;
+// document
+// 	.querySelector('.search-bar')
+// 	.addEventListener('keyup', function (event) {
+// 		if (event.key == 'Enter') {
+// 			weather.search();
+// 		}
+// 	});
 
-            fetch(api)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    const { temp } = data.main;
-                    const { description } = data.weather[0];
-                    const { name } = data;
-                    const { icon } = data.weather[0];
-                    // Set DOM elements from the API
-                    temperatureDegree.textContent = temp;
-                    temperatureDescription.textContent = description;
-                    locationTimezone.textContent = name;
-                    // Formula for Celsius
-                    let celsius = (temp - 32) * (5 / 9);
-
-                    // Set Icon
-                    setIcons(icon, document.querySelector('.icon'));
-
-                    // Change temperature to Celsius/Farenheit
-                    temperatureSection.addEventListener('click', () => {
-                        if (temperatureSpan.textContent === "F") {
-                            temperatureSpan.textContent = "C";
-                            temperatureDegree.textContent = Math.floor(celsius);
-                        } else {
-                            temperatureSpan.textContent = "F";
-                            temperatureDegree.textContent = temp;
-                        }
-                    });
-                });
-		});
-	}
-
-    // Function to set icons
-    function setIcons(icon, iconID) {
-        const skycons = new Skycons({ color: 'white' });
-        const currentIcon = icon.replace(/-/g, '_').toUpperCase();
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]);
-    }
-});
+// weather.fetchWeather('Denver');
